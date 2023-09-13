834ec97d67b27829355bf71bca4819c7236c1b42
QuadnucYard <2380433991@qq.com>
2023-09-13 15:01:04 +0800

    Fix gauge

--- src/views/admin/chat/components/FeedbackGauge.vue
+++ src/views/admin/chat/components/FeedbackGauge.vue
@@ -20,19 +20,19 @@ const chartRef = ref<InstanceType<typeof VChart>>();
 
 const gaugeData = [
   {
-    value: 20,
+    value: (props.data.total_like / props.data.total) * 100,
     name: "👍",
     title: { offsetCenter: ["0%", "-40%"] },
     detail: { valueAnimation: true, offsetCenter: ["0%", "-25%"] },
   },
   {
-    value: 40,
+    value: (props.data.total_dislike / props.data.total) * 100,
     name: "👎",
     title: { offsetCenter: ["0%", "-5%"] },
     detail: { valueAnimation: true, offsetCenter: ["0%", "10%"] },
   },
   {
-    value: 60,
+    value: (props.data.total_comment / props.data.total) * 100,
     name: "💬",
     title: { offsetCenter: ["0%", "30%"] },
     detail: { valueAnimation: true, offsetCenter: ["0%", "45%"] },
@@ -70,9 +70,9 @@ const option: EChartsOption = {
 };
 
 watch(props, () => {
-  gaugeData[0].value = props.data.total_like_today;
-  gaugeData[1].value = props.data.total_dislike_today;
-  gaugeData[2].value = props.data.total_comment_today;
+  gaugeData[0].value = (props.data.total_like / props.data.total) * 100;
+  gaugeData[1].value = (props.data.total_dislike / props.data.total) * 100;
+  gaugeData[2].value = (props.data.total_comment / props.data.total) * 100;
   chartRef.value?.setOption({ series: [{ data: gaugeData }] });
 });
 </script>
