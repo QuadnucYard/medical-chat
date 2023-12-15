54217ea97c08f6d4c67b3bfded69a5bffa6a137d
QuadnucYard <2380433991@qq.com>
2023-12-15 10:08:05 +0800

    Update packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
